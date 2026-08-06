1012f72840a0a985a21eba26d03d4fd591128a0d
Jacob Kolding <dacobi@gmail.com>
2026-08-06 12:08:16 +0200

    cars.lua, hover works, plasma background

--- cars.lua
+++ cars.lua
@@ -1,3 +1,5 @@
+setBG("[plasma:3]")
+
 addBouncer("[fontsize:2.0][pos:80,100][rect:400,150][center]Game[/center]")
 addBouncer("[pos:80,250][rect:400,300][hover:255,255,255][clicked:carcurves.lua][video:track.ogv]")
 
